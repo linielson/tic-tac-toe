a75796a93fb07a6353d8804551bf426a3c18d07a
Linielson Rosa <linielsonrosa@gmail.com>
2020-01-15 09:55:41 -0300

    Refactore square generator

--- src/index.js
+++ src/index.js
@@ -11,33 +11,36 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-  renderSquare(i) {
+  renderSquare(index) {
     return (
       <Square
-        value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}
+        key={index}
+        value={this.props.squares[index]}
+        onClick={() => this.props.onClick(index)}
       />
     )
   }
 
+  renderCol(square) {
+    return (this.renderSquare(square))
+  }
+
+  renderRow(row) {
+    const cols = [...Array(3).keys()]
+
+    return (
+      <div key={row} className='board-row'>
+        {cols.map((col) => { return this.renderCol(col + (row * cols.length)) })}
+      </div>
+    )
+  }
+
   render() {
+    const rows = [...Array(3).keys()]
+
     return (
       <div>
-        <div className='board-row'>
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className='board-row'>
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className='board-row'>
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {rows.map((row) => { return this.renderRow(row) })}
       </div>
     )
   }
